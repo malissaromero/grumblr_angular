5b23b4fdbaf9961bc8fbf4e5e35b9401bc1b9f42
Jesse Shawl <jesse@jshawl.com>
2015-09-01 11:32:04 -0400

    edit/update working

--- js/controllers/grumbles.js
+++ js/controllers/grumbles.js
@@ -14,6 +14,25 @@
 	content: this.content,
 	photoUrl: this.photoUrl
       })
+      this.title = ""
+      this.authorName = ""
+      this.content = ""
+      this.photoUrl = ""
+      // above 4 lines feel hacky. tried `.$setPristine()` is there a better way?
+    }
+    this.edit = function(index){
+      var grumble = this.grumbles[index] 
+      this.title = grumble.title
+      this.authorName = grumble.authorName
+      this.photoUrl = grumble.photoUrl
+      this.content = grumble.content
+    }
+    this.update = function(index){
+      var grumble = this.grumbles[index] 
+      grumble.title = this.title
+      grumble.authorName = this.authorName
+      grumble.photoUrl = this.photoUrl
+      grumble.content = this.content
     }
   });
 })();
